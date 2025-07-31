5560b0ad6adc702008116591740cf07022f5ff40
thugcreeper <nhpss100032@gmail.com>
2025-07-31 10:38:55 +0800

    部署的一些作業
    修改fetchSightJson中fetch的網站(改成render部署的網址看看

--- frontend/src/fetchSightJson.jsx
+++ frontend/src/fetchSightJson.jsx
@@ -1,7 +1,7 @@
 function fetchSightData(district) {
   let hint = document.getElementById('searchingHint'); // Show searching hint
   if(hint) hint.style.display = 'block';
-  return fetch(`http://127.0.0.1:8080/SightAPI?zone=${district}`, { method: "GET" })
+  return fetch(`https://keelungsightweb.onrender.com/SightAPIzone=${district}`, { method: "GET" })
     .then(response => {
       if (!response.ok) throw new Error('Network response was not ok');
       if(hint)hint.style.display = 'none'; // Hide searching hint
